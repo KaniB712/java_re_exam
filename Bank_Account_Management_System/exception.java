13d10a5e7822e3f64b7fa37f13e4972e676b5a6c
KaniB712 <129009754+KaniB712@users.noreply.github.com>
2025-02-05 17:57:04 +0530

    Delete Bank_Account_Management_System directory

--- Bank_Account_Management_System/exception.java
+++
@@ -1,7 +0,0 @@
-package bankManagement;
-@SuppressWarnings("serial")
-class InsufficientFundsException extends Exception {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
